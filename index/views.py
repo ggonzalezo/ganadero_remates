33ad47d74c906dbc5cbcc88c4fe198ac1a22efb5
ggonzalez <guigonzalez@alumnos.uai.cl>
2018-08-19 03:04:29 -0300

    Reparacion Front y Utils.py

--- index/views.py
+++ index/views.py
@@ -11,24 +11,24 @@ from django import db
 import heapq
 
 def index(request):
-	if request.method == 'POST':
-		form = AnimalForm(request.POST)
-		if form.is_valid():
-			animal = form.cleaned_data['animal']
-			ciudad = form.cleaned_data['ciudad']
-			feria = form.cleaned_data['feria']
-			result_json = estadistica(request, ciudad, feria, animal)
-			return HttpResponse(result_json, content_type='application/json')
-	else:
-		form = AnimalForm()
-	
-	return render(request, 'index.html', {'form': form})
+    if request.method == 'POST':
+        form = AnimalForm(request.POST)
+        if form.is_valid():
+            animal = form.cleaned_data['animal']
+            ciudad = form.cleaned_data['ciudad']
+            feria = form.cleaned_data['feria']
+            result_json = estadistica(request, ciudad, feria, animal)
+            return HttpResponse(result_json, content_type='application/json')
+    else:
+        form = AnimalForm()
+
+    return render(request, 'index.html', {'form': form})
 
 
 def estadistica(request,ciudad, feria, animal):
 
-	result = estadistica_basic(request, ciudad, feria, animal)
+    result = estadistica_basic(request, ciudad, feria, animal)
 
-	result = json.dumps(result)
+    result = json.dumps(result)
 
-	return result
+    return HttpResponse(result, content_type="application/json")
