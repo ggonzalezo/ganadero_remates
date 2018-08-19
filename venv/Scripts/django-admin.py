bf457cd7ba395f2937f941d7c3a958db86a0cc31
ggonzalez <guigonzalez@alumnos.uai.cl>
2018-08-19 00:37:31 -0300

    Start

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\guillermo\pycharmprojects\ganadero_remates\ganadero_remates\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
